4df1aaca921de8d72163d850c03f028b3bba9aaf
sangeetha@ <sangita708594@gmail.com>
2026-05-07 15:48:43 +0530

    Initial commit → add initial Spring Boot project setup

---
+++ sql-generator/src/main/java/com/sqlgen/SqlGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.sqlgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlGeneratorApplication.class, args);
+	}
+
+}
---
+++ sql-generator/src/test/java/com/sqlgen/SqlGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqlgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
