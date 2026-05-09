f3c7bcc300d2aab22d87519f4b3c333b7d19fc60
sangeetha@ <sangita708594@gmail.com>
2026-05-09 13:10:59 +0530

    add SqlController REST endpoint

---
+++ sql-generator/src/main/java/com/sqlgen/SqlController.java
@@ -0,0 +1,18 @@
+package com.sqlgen;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+public class SqlController {
+
+    @Autowired
+    private SqlGeneratorService sqlGeneratorService;
+
+    @PostMapping("/generate")
+    public SqlResponse generateSql(@RequestBody SqlRequest request) {
+        return sqlGeneratorService.generateSql(request.getPrompt());
+    }
+}
